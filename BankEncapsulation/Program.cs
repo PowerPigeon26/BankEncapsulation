66999b9a6304818437dcd309dbd6ea4b3d4d8616
Maverick Grayer <maverick72032@gmail.com>
2023-11-16 09:28:30 -0500

    Assignment complete

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account1 = new BankAccount();
+            double deposit;
+            bool conBanking;
+
+            Console.WriteLine($"Your current balance is: ");
+            account1.GetBalance();
+            Console.WriteLine("Would you like to make a deposit? (yes or no)");
+            Console.WriteLine("If not, your account balance will be given.");
+            conBanking = Console.ReadLine() == "yes";
+            while ( conBanking )
+            {
+                Console.WriteLine("How much would you like to deposit?");
+                while (double.TryParse(Console.ReadLine(), out deposit) != true)
+                {
+                    Console.WriteLine("Not a valid amount to add.");
+                }
+                account1.Deposit(deposit);
+                Console.WriteLine("Okay, your new balance is: ");
+                account1.GetBalance();
+                Console.WriteLine("Would you like to make another deposit? (yes or no).");
+                conBanking = Console.ReadLine() == "yes";
+            }
+                
+            Console.WriteLine($"Okay, your account balance is shown below. Bye!");
+            account1.GetBalance();
+            
         }
     }
 }
